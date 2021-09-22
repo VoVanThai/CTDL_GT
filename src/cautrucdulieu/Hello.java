87d1ed714cdfc1f3bc0e117716219e7dad735fb3
VoVanThai <vovanthai100802@gmail.com>
2021-09-22 11:00:31 +0700

    Nộp bài tuần 3

--- src/cautrucdulieu/Hello.java
+++ src/cautrucdulieu/Hello.java
@@ -2,7 +2,7 @@ package cautrucdulieu;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello");      
     }
     
 }
---
+++ src/com/thaivv2004110036/tuan3/Bai1Lab3.java
@@ -0,0 +1,26 @@
+package com.thaivv2004110036.tuan3;
+
+import java.util.Scanner;
+
+public class Bai1Lab3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập vào số n : ");
+        int n = sc.nextInt();
+        kiemTraSoNuyenTo(n);
+    }
+
+    public static void kiemTraSoNuyenTo(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                count++;
+        }
+
+        if (count > 2)
+            System.out.println("Không phải là số nguyên tố");
+        else
+            System.out.println("Là số nguyên tố");
+
+    }
+}
