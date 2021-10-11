a5ad2f047bd248c971bb37636a37a52c84895dbf
VoVanThai <vovanthai100802@gmail.com>
2021-10-11 07:17:44 +0700

    update

--- src/com/thaivv2004110036/tuan4/Bai1Lab4.java
+++ src/com/thaivv2004110036/tuan4/Bai1Lab4.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Bai1Lab4 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> a = new ArrayList<>();
+        while (true) {
+            double x = sc.nextDouble();
+            a.add(x);
+            System.out.println("Nhập thêm (Y/N)?");
+            if(sc.nextLine().equals("N"))
+                break;
+        }
+        for(Double i : a)
+            System.out.println(i);
     }
 }
