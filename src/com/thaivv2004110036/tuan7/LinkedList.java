db77823675a027eab163f1e2eb738b25c728c327
VoVanThai <vovanthai100802@gmail.com>
2021-10-21 21:20:30 +0700

    update

---
+++ src/com/thaivv2004110036/tuan7/LinkedList.java
@@ -0,0 +1,32 @@
+
+public class LinkedList {
+    Node head = null;
+    Node tail = null;
+
+
+void add(int data) {
+    Node newNode = new Node(data);
+    if(head == null) {
+        head = newNode;
+        tail = newNode;
+    } else {
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+}
+
+ void print() {
+    Node current = head;
+    if(head == null) {
+        System.out.println("Danh sách rỗng.");
+    }
+    
+    System.out.println("Các nút trong danh sách: ");
+    while(current != null) {
+        System.out.println(current.data);
+        current = current.next;
+    }
+}
+
+}
